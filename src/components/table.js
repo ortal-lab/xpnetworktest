65d47c4f1736e915661e8e62dfede400fb25555f
ortal-lab <ortal0166@gmail.com>
2021-09-12 16:33:09 +0300

    try2

--- src/components/table.js
+++ src/components/table.js
@@ -100,62 +100,49 @@ const Row = ({ row, index, moveRow }) => {
     const dropRef = React.useRef(null);
     const dragRef = React.useRef(null);
 
-    // const [, drop] = useDrop({
-    //     accept: DND_ITEM_TYPE,
-    //     hover(item, monitor) {
-    //         if (!dropRef.current) {
-    //             return;
-    //         }
-    //         const dragIndex = item.index;
-    //         const hoverIndex = index;
-    //         // Don't replace items with themselves
-    //         if (dragIndex === hoverIndex) {
-    //             return;
-    //         }
-    //         // Determine rectangle on screen
-    //         const hoverBoundingRect = dropRef.current.getBoundingClientRect();
-    //         // Get vertical middle
-    //         const hoverMiddleY =
-    //             (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
-    //         // Determine mouse position
-    //         const clientOffset = monitor.getClientOffset();
-    //         // Get pixels to the top
-    //         const hoverClientY = clientOffset.y - hoverBoundingRect.top;
-    //         // Only perform the move when the mouse has crossed half of the items height
-    //         // When dragging downwards, only move when the cursor is below 50%
-    //         // When dragging upwards, only move when the cursor is above 50%
-    //         // Dragging downwards
-    //         if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
-    //             return;
-    //         }
-    //         // Dragging upwards
-    //         if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
-    //             return;
-    //         }
-    //         // Time to actually perform the action
-    //         moveRow(dragIndex, hoverIndex);
-    //         // Note: we're mutating the monitor item here!
-    //         // Generally it's better to avoid mutations,
-    //         // but it's good here for the sake of performance
-    //         // to avoid expensive index searches.
-    //         item.index = hoverIndex;
-    //     },
-    // });
+    const [, drop] = useDrop({
+        accept: DND_ITEM_TYPE,
+        hover(item, monitor) {
+            if (!dropRef.current) {
+                return;
+            }
+            const dragIndex = item.index;
+            const hoverIndex = index;
+            if (dragIndex === hoverIndex) {
+                return;
+            }
+            const hoverBoundingRect = dropRef.current.getBoundingClientRect();
+            const hoverMiddleY =
+                (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
+            const clientOffset = monitor.getClientOffset();
+            const hoverClientY = clientOffset.y - hoverBoundingRect.top;
 
-    // const [{ isDragging }, drag, preview] = useDrag({
-    //     item: { type: DND_ITEM_TYPE },
-    //     collect: (monitor) => ({
-    //         isDragging: monitor.isDragging(),
-    //     }),
-    // });
+            if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
+                return;
+            }
+            if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
+                return;
+            }
+            moveRow(dragIndex, hoverIndex);
+            item.index = hoverIndex;
+        },
+    });
 
-     //const opacity = isDragging ? 0 : 1;
+    const [{ isDragging }, drag, preview] = useDrag({
+        item: { type: DND_ITEM_TYPE, index },
+        type: DND_ITEM_TYPE,
+        collect: (monitor) => ({
+            isDragging: monitor.isDragging(),
+        }),
+    });
 
-    // preview(drop(dropRef));
-    // drag(dragRef);
+    const opacity = isDragging ? 0 : 1;
+
+    preview(drop(dropRef));
+    drag(dragRef);
 
     return (
-        <tr ref={dropRef} >
+        <tr ref={dropRef}>
             <td ref={dragRef}>move</td>
             {row.cells.map((cell) => {
                 return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>;
@@ -197,7 +184,7 @@ const TableA = () => {
     );
 
     const data = React.useMemo(() => users, []);
-console.log(data);
+    console.log(data);
     return (
         <Styles>
             <Table columns={columns} data={data} />
